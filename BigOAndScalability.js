df2bfdc932128a71ea15c8d30183aef5c21a2b35
Bruno Machado <brunomdr46@gmail.com>
2023-10-09 23:06:25 -0300

    Solution: Big O Calculation

--- BigOAndScalability.js
+++ BigOAndScalability.js
@@ -26,3 +26,19 @@ function findNemo(array) {
 
 // O(n) --> Linear Time
 findNemo(large); 
+
+boxes = ['Samsung', 'iPhone', 'Xiaomi']
+
+function compressFirstBox(boxes) {
+  console.log(boxes[0])
+}
+
+// O(1) --> Constant Time
+compressFirstBox(boxes)
+
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]) // O(1)
+  console.log(boxes[1]) // O(1)
+}
+
+logFirstTwoBoxes(boxes) // O(2) --> Constant Time
